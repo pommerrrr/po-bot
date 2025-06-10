97c7aeb6448ee996574bb3b159c159cc1b1be950
pommerrrr <119147432+pommerrrr@users.noreply.github.com>
2025-06-10 17:02:38 -0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,28 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     host: '0.0.0.0',
     port: 5173
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    target: 'es2015',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   },
   resolve: {
     alias: {
       "@": "/src",
     },
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false
-  }
 })
